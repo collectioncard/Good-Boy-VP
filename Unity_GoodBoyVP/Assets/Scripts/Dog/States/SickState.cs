44ddd4198b584f5594758cce831adc053e00dbd9
Jacqueline <jacqueline.gracey@gmail.com>
2024-08-20 15:25:18 -0700

    sick state (no meta file)

---
+++ Unity_GoodBoyVP/Assets/Scripts/Dog/States/SickState.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityHFSM;
+
+namespace Dog.States
+{
+    public class SickState : StateBase
+    {
+        private DogState dogState;
+
+        public SickState(DogState state) : base(needsExitTime: false, isGhostState: false)
+        {
+            this.dogState = state;
+        }
+
+        public override void OnEnterEnter()
+        {
+            Debug.Log("Dog is now sick.");
+        }
+
+        public override void OnLogic()
+        {
+
+            dogState.Health -= Time.deltaTime * 5;  // Decrease health faster when sick
+
+            // Maybe prevent the dog from playing
+            if (dogState.Health <= 0)
+            {
+                // Death
+            }
+        }
+
+        public override void OnExit()
+        {
+            Debug.Log("Dog is no longer sick.");
+            // Logic for when the dog recovers from sickness
+        }
+    }
+}
